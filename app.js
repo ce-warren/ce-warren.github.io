b3395077dd018543538875d7c5af5a4a8a988062
Christina Warren <44888733+ce-warren@users.noreply.github.com>
2026-04-02 02:36:06 -0700

    test

--- app.js
+++ app.js
@@ -248,27 +248,7 @@ function App() {
           <p>{`Enemy health: ${enemyHealth}`}</p>
           <p>{`Shield: ${shield}`}</p>
         </div>
-        {currentEnemy && (
-          <div>
-            <Card card={currentEnemy} />
-          </div>
-        )}
         
-        {phase === PHASE.ATTACK && (
-          <div>
-           <p>Attack</p>
-          <div>
-        )}
-        {phase === PHASE.REPLENISH || phase === PHASE.ATTACK || phase === PHASE.DEFEND && (
-          <div>
-            <p>Played cards</p>
-            {playCards.map(card => (
-              <Card
-                card={card}
-               />
-            ))}
-          <div>
-        )}
         <div>
           <p>{`Hand - ${HAND_TRACKER[currentHand].name}`}</p>
           {HAND_TRACKER[currentHand].hand.map(card => (
